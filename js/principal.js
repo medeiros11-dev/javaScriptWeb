2b17890d209819e1f6351697a291da7cc18b7247
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-07-15 20:44:32 -0300

    JS

--- js/principal.js
+++ js/principal.js
@@ -3,29 +3,34 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
 var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
+var tdPeso = paciente.querySelector(".info-peso");
 var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
 var alturaEhValida = true;
-if (peso <= 0 || peso >= 1000) {
+var pesoEhValido = true;
+
+if (peso <= 0 || peso > 1000) {
     console.log("Peso inválido!");
+    tdPeso.textContent = "Peso inválido!";
     pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
+
 }
 
-if (altura <= 0 || altura >= 3.00){
+if (altura <= 0 || altura >= 3) {
     console.log("Altura inválida!");
+    tdAltura.textContent = "Altura inválida!";
     alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
 }
 
-if (alturaEhValida && pesoEhValido){
+if (alturaEhValida && pesoEhValido) {
+
     var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    tdImc.textContent = imc;    
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
 }
