89a2e645004429360a6a0dfc893d21a572cd9f07
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-07-18 21:45:21 -0300

    JS

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,41 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdImc = paciente.querySelector(".info-imc");
+for(var i=0; i<pacientes.length; i++){
+    paciente = pacientes[i];
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdImc = paciente.querySelector(".info-imc");
 
 
-var altura = tdAltura.textContent;
-var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-if (peso <= 0 || peso > 1000) {
-    console.log("Peso inválido!");
-    tdPeso.textContent = "Peso inválido!";
-    pesoEhValido = false;
+    if (peso <= 0 || peso > 500) {
+        console.log("Peso inválido!");
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.style.color = "red";
 
-}
+    }
 
-if (altura <= 0 || altura >= 3) {
-    console.log("Altura inválida!");
-    tdAltura.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida!");
+        tdAltura.textContent = "Altura inválida!";
+        alturaEhValida = false;
+    }
 
-if (alturaEhValida && pesoEhValido) {
+    if (alturaEhValida && pesoEhValido) {
 
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;    
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!"
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);    
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
 }
